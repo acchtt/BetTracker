e5c079b84b3f143781d432921bb53beade49c33a
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-07-27 00:56:51 +0700

    Load refined metadata detection

--- parser-popup.js
+++ parser-popup.js
@@ -166,6 +166,7 @@
 
   [
     "bet-metadata.js?v=20260727-1",
+    "bet-metadata-fixes.js?v=20260727-1",
     "sport-detection-patch.js?v=20260727-1",
     "live-sync.js?v=20260727-3",
     "manual-popup.js?v=20260727-4",
